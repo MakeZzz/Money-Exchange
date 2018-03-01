eab79baa6ba18c289acd7f80035e48895d531800
MakeZzz <maxim1080@gmail.com>
2018-03-01 19:00:55 +0300

    master/feacher: currency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  var currency , obj, H=0,  Q=0, D=0,N=0, P=0,  age1
+  a = currency
+
+while (a - 50 >= 50) {
+H = H++
+}
+while (a-25 >= 25){
+Q = Q++
+}
+while ( a - 10 >= 10) {
+D = D++
+}
+while (a - 5 >= 5) {
+N = N++
+}
+while (a - 1>=1) {
+P = P++
+}
+while (a == 0) {
+  break
+}
+}
+
+var obj = {
+name1 : "H"
+age1 : Number(H)
+name2 : "P"
+age : P
+name3 : "Q"
+age: Q
+name4: "N"
+age: N
+name5: "M"
+age: M
+}
+return (a): (obj.name1 : obj.age1, obj.name2 : obj.nominal2, obj.name3 : obj.nomnal3, obj.name4 : obj.nominal4, obj.name5 : obj.nominal5)
 }
